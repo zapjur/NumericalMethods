be772ada77f7366662fa8cadd277ddf0cd1fb292
Piotr Zapiór <piotrzapior0@gmail.com>
2023-11-08 18:50:29 +0100

    dodana rekurencja w 4_5

--- Lab_04/Zad4_5.m
+++ Lab_04/Zad4_5.m
@@ -2,10 +2,32 @@ clear all;
 close all;
 clc;
 
-matrix = [1,2,3; 3,4,5; 6,6,5];
+matrix = [3,2,8,9; 6,8,9,3; 5,8,8,6; 3,5,7,8];
 iterative = (1/det(matrix)) * calculateAdjointMatrixIterative(matrix)
 inv(matrix)
+rec = (1/det(matrix))* calculateAdjointMatrixRec(matrix, zeros(size(matrix)), 1, 1)
 
+%Podejście rekurencyjne
+function adjointMatrixF = calculateAdjointMatrixRec(matrix, adjointMatrix, i, j)
+    [rows, cols] = size(matrix);
+
+    minor = calculateMinorRec(matrix, i, j);
+    adjointMatrix(j, i) = (-1)^(i+j) * minor;
+    
+    if(i >= rows && j >= cols)
+        adjointMatrixF = adjointMatrix;
+    elseif(j >= cols)
+        adjointMatrixF = calculateAdjointMatrixRec(matrix, adjointMatrix, i+1, 1);
+    else
+        adjointMatrixF = calculateAdjointMatrixRec(matrix, adjointMatrix, i, j+1);
+    end
+end
+
+function minorMatrix = calculateMinorRec(A,row, col)
+
+    minorMatrix = det(A([1:row-1, row+1:end], [1:col-1, col+1:end]));
+
+end
 
 %Podejście iteracyjne
 function adjointMatrix = calculateAdjointMatrixIterative(A)
